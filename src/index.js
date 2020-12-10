f43c9115ebdad0e73e31cf9dbfde1a7628594b99
recafox <recafox@gmail.com>
2020-12-10 21:40:43 +0800

    panel style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import hello from './hello.js'
-import Button from './component/Button/button.js';
-import Heading from './component/Heading/heading.js'
+// import hello from './hello.js'
+// import Button from './component/Button/button.js';
+// import Heading from './component/Heading/heading.js'
 
 require('/src/style/style.scss');
 
+let actionBtns = document.querySelectorAll('.js-action');
+let actionEffect = function (e, node) {
+  e.stopPropagation();
+  let target = e.target;
+  console.log(target);
 
-
-if (process.env.NODE_ENV === 'production') {
-  console.log("this is production mode");
 }
+
+actionBtns.forEach(function (item) {
+  console.log(item);
+  item.addEventListener('click', actionEffect);
+})
